195df5b4c79f6f84f87ffa06c89f2bb5a783a7b8
zhu <zhu@mail.com>
2021-12-16 15:40:45 +0900

    add read answer

--- lib/readfile.c
+++ lib/readfile.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 
+#define FOPEN_ERROR -1
+#define SUCCESS 1
+
 int read_data(char filename[], char dat1[], char dat2[]){
     FILE *fp;
     fp = fopen(filename, "r");
-    if(fp == NULL) return -1;
+    if(fp == NULL) return FOPEN_ERROR;
 
     fscanf(fp, "%s%s", dat1, dat2);
 
     fclose(fp);
-    return 1;    
+    return SUCCESS;
+}
+
+int read_answer(char filename[], int *dat1, int *dat2){
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if(fp == NULL) return FOPEN_ERROR;
+
+    fscanf(fp, "%d%d", dat1, dat2);
+
+    fclose(fp);
+    return SUCCESS;
 }
 
 int main(){
