22aa93be5f1c1fa4275f7b50cc46bfae2965526f
zhu <zhu@mail.com>
2021-12-20 18:55:14 +0900

    init

---
+++ lib/edata_generator.c
@@ -0,0 +1,15 @@
+// edata generator do not guarantee the correct answer
+#include <stdio.h>
+#include <string.h>
+
+/* 
+文字列a, bのedataを求める
+正解が連続しているときに精度が良い
+a: 正解のchar配列
+b: 観測値char配列
+na: aの長さ
+nb: bの長さ
+*/
+int edata(char a[], int na, char b[], int nb){
+    
+}
