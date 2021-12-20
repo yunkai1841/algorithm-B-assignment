05e5c391a4138101b1c90a29ea06fa8719c6048c
zhu <zhu@mail.com>
2021-12-20 22:27:15 +0900

    unfinish

--- lib/edata_generator.c
+++ lib/edata_generator.c
@@ -1,6 +1,9 @@
 // edata generator do not guarantee the correct answer
 #include <stdio.h>
 #include <string.h>
+#include "lib.h"
+
+#define TARGET_PER_STEP 10
 
 /* 
 文字列a, bのedataを求める
@@ -9,7 +12,33 @@ a: 正解のchar配列
 b: 観測値char配列
 na: aの長さ
 nb: bの長さ
+result: 結果格納配列
 */
-int edata(char a[], int na, char b[], int nb){
-    
+int edata(char a[], int na, char b[], int nb, char result[]){
+    int i, j;
+    int flag = 0;
+    for(i = 0; i < na; i++){
+        int correct =editdis(a+i, TARGET_PER_STEP, b+flag, TARGET_PER_STEP);
+        int yokei = editdis(a+i, TARGET_PER_STEP, b+flag+1, TARGET_PER_STEP);
+        int sobire = editdis(a+i+1, TARGET_PER_STEP, b+flag, TARGET_PER_STEP);
+        // int miss = editdis(s+i+1, TARGET_PER_STEP, b+flag+1, TARGET_PER_STEP);
+
+        // 判別
+        if(yokei < correct && yokei <= sobire){
+            // yokei
+            result[i] = 'i'; flag+=2;
+        } else if(sobire < correct && sobire <= yokei){
+            // sobire
+            result[i] = 'd';
+        } else{
+            // correct or miss
+            if (a[i] == b[flag]){
+                result[i] = '.'; flag++;
+            } else {
+                result[i] = 's'; flag++;
+            }            
+        }
+
+        // printf("answer>%c, my>%c\n", edata_correct[i], result[i]);
+    }
 }
