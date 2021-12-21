909c0d494e9b0efc4d73688b1c472ddde243c6be
zhu <zhu@mail.com>
2021-12-21 20:52:27 +0900

    add edata function

--- lib/lib.h
+++ lib/lib.h
@@ -1,3 +1,4 @@
 int editdis(char a[], int na, char b[], int nb);
 int read_data(char filename[], char dat1[], char dat2[]);
 int read_answer(char filename[], int *dat1, int *dat2);
+int edata(char a[], int na, char b[], int nb, char result[]);
